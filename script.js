4589775a4fcd189c00d6bcf5f4ca64112d7234e9
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-06-19 13:01:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	arr.sort((a, b) => a - b); // Sort the array in ascending order
+
+  let closestSum = Infinity; // Variable to track the closest sum found
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        // Found exact sum equal to target
+        return sum;
+      }
+    }
+  }
+  return closestSum;
 }
 
 module.exports = threeSum;
